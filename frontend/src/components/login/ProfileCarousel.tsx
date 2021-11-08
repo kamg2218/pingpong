f81d6e2702e89fea2517e4a29aafcb638448040f
윤현지 <hyoon@student.42seoul.kr>
2021-11-08 18:01:39 +0900

    update nickandprofile page

--- frontend/src/components/login/ProfileCarousel.tsx
+++ frontend/src/components/login/ProfileCarousel.tsx
@@ -3,10 +3,14 @@ import profileImage2 from '../../icons/profileImage2.png';
 import profileImage3 from '../../icons/profileImage3.png';
 import profileImage4 from '../../icons/profileImage4.png';
 import profileImage5 from '../../icons/profileImage5.png';
+import profileImage6 from '../../icons/profileImage6.png';
+import profileImage7 from '../../icons/profileImage7.png';
+import profileImage8 from '../../icons/profileImage8.png';
+import profileImage9 from '../../icons/profileImage9.png';
 import '../../css/ProfileCarousel.css';
 
 export default function ProfileCarousel(props: any){
-    const maxNum:number = 4;
+    const maxNum:number = 4 + 4;
 
     return (
         <div id="carouselIndicators" className="carousel slide" data-bs-touch='false' data-bs-interval='false'>
@@ -16,22 +20,38 @@ export default function ProfileCarousel(props: any){
                 <button data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3" onClick={()=>props.setProfile(2)}></button>
                 <button data-bs-target="#carouselIndicators" data-bs-slide-to="3" aria-label="Slide 4" onClick={()=>props.setProfile(3)}></button>
                 <button data-bs-target="#carouselIndicators" data-bs-slide-to="4" aria-label="Slide 5" onClick={()=>props.setProfile(4)}></button>
+                <button data-bs-target="#carouselIndicators" data-bs-slide-to="5" aria-label="Slide 6" onClick={()=>props.setProfile(5)}></button>
+                <button data-bs-target="#carouselIndicators" data-bs-slide-to="6" aria-label="Slide 7" onClick={()=>props.setProfile(6)}></button>
+                <button data-bs-target="#carouselIndicators" data-bs-slide-to="7" aria-label="Slide 8" onClick={()=>props.setProfile(7)}></button>
+                <button data-bs-target="#carouselIndicators" data-bs-slide-to="8" aria-label="Slide 9" onClick={()=>props.setProfile(8)}></button>
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item profile active">
-                    <img src={profileImage1} className= "d-block img" alt="..."/>
+                    <img src={profileImage1} className="center-block img" alt="..."/>
                 </div>
                 <div className="carousel-item profile">
-                    <img src={profileImage2} className="d-block img" alt="..."/>
+                    <img src={profileImage2} className="center-block img" alt="..."/>
                 </div>
                 <div className="carousel-item profile">
-                    <img src={profileImage3} className="d-block img" alt="..."/>
+                    <img src={profileImage3} className="center-block img" alt="..."/>
                 </div>
                 <div className="carousel-item profile">
-                    <img src={profileImage4} className="d-block img" alt="..."/>
+                    <img src={profileImage4} className="center-block img" alt="..."/>
                 </div>
                 <div className="carousel-item profile">
-                    <img src={profileImage5} className="d-block img" alt="..."/>
+                    <img src={profileImage5} className="center-block img" alt="..."/>
+                </div>
+                <div className="carousel-item profile">
+                    <img src={profileImage6} className="center-block img" alt="..."/>
+                </div>
+                <div className="carousel-item profile">
+                    <img src={profileImage7} className="center-block img" alt="..."/>
+                </div>
+                <div className="carousel-item profile">
+                    <img src={profileImage8} className="center-block img" alt="..."/>
+                </div>
+                <div className="carousel-item profile">
+                    <img src={profileImage9} className="center-block img" alt="..."/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev" onClick={()=>{props.setProfile(!props.profile ? maxNum : props.profile - 1)}}>
--- frontend/src/components/login/nick.tsx
+++ frontend/src/components/login/nick.tsx
@@ -7,14 +7,15 @@ const url = 'http://localhost:4242';
 
 export default function Nick(){
     const [profile, setProfile] = useState<number>(0);
-    const [nickname, setNickname] = useState<String>("");
+    const [nickname, setNickname] = useState<string>("");
+    const nicknamePlaceholder:string = "2~12 characters only";
 
     function handleCheck(event : any){
         event.preventDefault();
         console.log(nickname);
         console.log(profile);
         axios.get(`${url}/auth/check?nickname=${nickname}`)
-        .then(res=>{console.log(res)})
+        .then(res=>{console.log(res.data)})
         .catch(error=>{console.log(error)});
     }
     function handleOK(event: any){
@@ -30,13 +31,35 @@ export default function Nick(){
         event.preventDefault();
         window.location.href = '/';
     }
+    const tab:number = -1;
 
     return (
-        <form className="col-sm-8 col-md-5">
+        <form>
             <Profile profile={profile} setProfile={setProfile}></Profile>
-            <div className="nick">
-                <input placeholder="nickname" onChange={(event)=>setNickname(event.target.value)}/>
-                <button className="btn btn-outline-dark m-1" onClick={handleCheck}>Check</button>
+            <div className="d-flex my-2">
+                <label className="nickLabel m-2">Nickname</label>
+                <input className="m-1" placeholder={nicknamePlaceholder} onChange={(event)=>setNickname(event.target.value)} required />
+                <button className="btn btn-outline-dark m-1" data-toggle="modal" data-target="#checkModal" onClick={handleCheck}>Check</button>
+
+                {/* modal */}
+                <div className="modal fade" id="checkModal" tabIndex={tab} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                ...
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">OK</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <button className="btn btn-outline-dark m-1" type="submit" onClick={handleOK}>OK</button>
