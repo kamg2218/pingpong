12265aa1bc26b54e5c7270a972eb806dbbfb2918
윤현지 <hyoon@student.42seoul.kr>
2021-11-17 10:43:53 +0900

    frontend login and signup pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './css/App.css';
 import Main from './pages/Main';
 import LoginError from './components/errors/LoginError';
-import Game from './pages/games/Game';
-import Admin from './pages/Admin';
 import NickAndProfile from './pages/login/NickAndProfile';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -14,8 +12,7 @@ function App() {
         <Route exact path='/'><Main /></Route>
         <Route path='/loginerror'><LoginError/></Route>
         <Route path='/user/nickandprofile'><NickAndProfile/></Route>
-        <Route path='/game'><Game/></Route>
-        <Route path='/admin'><Admin/></Route>
+        <Route path='/game'></Route>
         <Route><Main/></Route>
       </Switch>
       {/* <footer id='footer'>@42seoul</footer> */}
--- frontend/src/pages/games/GameRoom.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../css/GameRoom.css';
-
-export default function GameRoom(){
-    return (
-        <div>
-        </div>
-    );
-}
