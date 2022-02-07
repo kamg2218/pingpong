e6e9d41df153cfb10bc7cf883cc6d981ff947800
윤현지 <hyoon@student.42seoul.kr>
2022-02-07 10:37:31 +0900

    :boom: update main branch

--- backend/myserver/src/config/redirect_url.ts
+++
@@ -1,8 +0,0 @@
-const front = "http://localhost:3000"
-
-const fronLoginPage = front + "/login"
-const frontSignupPage = front + "/signup"
-const frontLobyPage = front + "/loby"
-const frontTwoFactorAuthenticationPage = front + "/2fa"
-
-export {fronLoginPage, frontSignupPage, frontLobyPage, frontTwoFactorAuthenticationPage}
--- backend/myserver/src/db/entity/Game/GameEntity.ts
+++
@@ -1,5 +0,0 @@
-import { GameHistory } from "./GameHistory.entity";
-import { GameRoom } from "./GameRoom.entity";
-import { GameRoomMembership } from "./GameRoomMembership.entity";
-
-export { GameHistory, GameRoom, GameRoomMembership }
