37e6f3b5a1c10e0ea9eca1d92176c9641e2e2baa
jikwon <jikwon@student.42seoul.kr>
2022-02-11 12:18:45 +0900

    backend docker compose

--- backend/myserver/src/config/ormconfig.ts
+++ backend/myserver/src/config/ormconfig.ts
@@ -2,7 +2,7 @@ import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 
 export const ormconfig : TypeOrmModuleOptions= {
    "type": "postgres",
-   "host": "localhost",
+   "host": "192.168.0.19",
    "port": 5432,
    "username": "postgres",
    "password": "12345",
