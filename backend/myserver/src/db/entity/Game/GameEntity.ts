874ede2f4c52f2af15d5ed5b295fd92aa54e7f81
jikwon <jikwon@student.42seoul.kr>
2022-02-14 11:16:17 +0900

    delete config module

--- backend/myserver/src/db/entity/Game/GameEntity.ts
+++
@@ -1,5 +0,0 @@
-import { GameHistory } from "./GameHistory.entity";
-import { GameRoom } from "./GameRoom.entity";
-import { GameMembership } from "./GameMembership.entity";
-
-export { GameHistory, GameRoom, GameMembership }
