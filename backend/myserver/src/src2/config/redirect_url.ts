cc75ee8bd58048308142d48bf1c40df0fbf2ba6a
jikwon <jikwon@student.42seoul.kr>
2022-02-18 11:36:21 +0900

    delete old folder

--- backend/myserver/src/src2/config/redirect_url.ts
+++
@@ -1,8 +0,0 @@
-const front = "http://localhost:3000"
-
-const fronLoginPage = front + "/game"
-const frontSignupPage = front + "/nickandprofile"
-const frontLobyPage = front + "/game"
-const frontTwoFactorAuthenticationPage = front + "/2fa"
-
-export {fronLoginPage, frontSignupPage, frontLobyPage, frontTwoFactorAuthenticationPage}
