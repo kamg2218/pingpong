73283f388a10ffdea4385fc4584c52b60a36f3f7
jikwon <jikwon@student.42seoul.kr>
2022-02-19 11:14:50 +0900

    createGameRoom

---
+++ backend/myserver/src/config/redirect_url.ts
@@ -0,0 +1,8 @@
+const front = "http://localhost:3000"
+
+const fronLoginPage = front + "/game"
+const frontSignupPage = front + "/nickandprofile"
+const frontLobyPage = front + "/game"
+const frontTwoFactorAuthenticationPage = front + "/2fa"
+
+export {fronLoginPage, frontSignupPage, frontLobyPage, frontTwoFactorAuthenticationPage}
---
+++ backend/myserver/src/db/entity/Chat/ChatEntity.ts
@@ -0,0 +1,6 @@
+import { ChatBanList } from "./ChatBanList.entity";
+import { ChatHistory } from "./ChatHistory.entity";
+import { ChatMembership } from "./ChatMembership.entity";
+import { ChatRoom } from "./ChatRoom.entity";
+
+export {ChatBanList, ChatHistory, ChatMembership, ChatRoom} 
