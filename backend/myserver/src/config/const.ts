0a3efcf80059db4970a137df7c198c6ccc6bf7b2
jikwon <jikwon@student.42seoul.kr>
2022-02-21 13:52:09 +0900

    parsing cookie

---
+++ backend/myserver/src/config/const.ts
@@ -0,0 +1,1 @@
+export const DELAYEDDAY = 7
---
+++ backend/myserver/src/events/online/onlineChatRoom.ts
@@ -0,0 +1,64 @@
+import { Server } from "socket.io";
+import { User } from "src/db/entity/User/UserEntity";
+import { BlockedFriendsRepository, UserRepository } from "src/db/repository/User/UserCustomRepository";
+import { AuthSocket } from "src/type/AuthSocket.interface";
+import { getCustomRepository } from "typeorm";
+import { onlineManager } from "./onlineManager";
+
+export class onlineChatRoom {
+    static server : Server = null;
+    private roomid : string;
+    private members : string[];
+    
+    get id() {
+        return this.roomid;
+    }
+
+    static init(server : Server) {
+        if (!onlineChatRoom.server)
+            onlineChatRoom.server = server;
+    }
+
+    constructor(roomid : string) {
+        this.roomid = roomid;
+        this.members = [];
+    }
+
+    public async sayToRoom(socket : AuthSocket, payload : any) {
+        const repo_blockList = getCustomRepository(BlockedFriendsRepository);
+        const repo_user = getCustomRepository(UserRepository);
+        let user = await repo_user.findOne(socket.userid);
+        this.members.map(async (socketid) => {
+            let theOtherId = onlineManager.userIdOf(socketid);
+            if (!await repo_blockList.amIBlockedByid(user, theOtherId))
+                onlineChatRoom.server.to(socketid).emit("chatMessage", payload);
+        });
+    }
+
+    public announceExceptMe(mySocketId : string, event : string, payload : any) {
+        this.members.map(async (socketid)=>{
+            if (socketid !== mySocketId)
+                onlineChatRoom.server.to(socketid).emit(event, payload);
+        });
+    }
+
+    public announce(event : string, payload : any) {
+        this.members.map(async (socketid)=>{
+            onlineChatRoom.server.to(socketid).emit(event, payload);
+        });
+    }
+
+    public join(socketid: string) {
+        if (socketid)
+            this.members.push(socketid);
+    }
+
+    public leave(socketid : string) {
+        if (!socketid)
+            return ;
+        const index = this.members.indexOf(socketid);
+        if (index !== -1)
+            this.members.splice(index, 1);
+    }
+    
+}
