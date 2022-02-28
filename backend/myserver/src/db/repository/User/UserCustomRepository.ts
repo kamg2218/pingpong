ca5c73095b39e03ea7713088e8f5234523b32f0d
jikwon <jikwon@student.42seoul.kr>
2022-02-28 11:17:06 +0900

    deltet old files

--- backend/myserver/src/db/repository/User/UserCustomRepository.ts
+++
@@ -1,5 +0,0 @@
-import { FriendsRepository } from "./Friends.repository";
-import { UserRepository } from "./User.repository";
-import { RestrictedListReopsitory } from "./RestrictedList.repository"
-import { BlockedFriendsRepository } from "./BlockFriends.repository";
-export { FriendsRepository, UserRepository, RestrictedListReopsitory, BlockedFriendsRepository }
