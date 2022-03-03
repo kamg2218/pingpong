0d747f014ac97c554e7ee336e2d6fe0cf47ce23e
jikwon <jikwon@student.42seoul.kr>
2022-03-03 11:02:55 +0900

    delete old files

--- backend/src/db/entity/User/UserEntity.ts
+++
@@ -1,6 +0,0 @@
-import { BlockedFriends } from './BlockedFriends.entity'
-import { Friends } from './Friends.entity'
-import { RestrictedList } from './RestrictedList.entity'
-import { User } from './User.entity'
-
-export { BlockedFriends, Friends, RestrictedList, User }
