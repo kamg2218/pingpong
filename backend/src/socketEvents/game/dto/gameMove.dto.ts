f8332d48bf19567c888a6f6a889c9f7232ecc750
jikwon <jikwon@student.42seoul.kr>
2022-03-13 16:34:19 +0900

    delete folder

--- backend/src/socketEvents/game/dto/gameMove.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString } from "class-validator";
-import { GameRoomInfoDTO } from "./gameRoomInfo.dto";
-
-export class GameMoveDTO extends GameRoomInfoDTO {
-    @IsString()
-    direction : string;
-}
--- backend/src/socketEvents/user/dto/user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { NewFriendDTO } from "./newFriend.dto";
-import { UserInfoDTO } from "./userInfo.dto";
-import { UpdateProfileDTO } from "./updateProfile.dto";
-export {NewFriendDTO, UserInfoDTO, UpdateProfileDTO};
