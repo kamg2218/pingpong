715e326983c9d4f4529de8c682f9304eaa44c245
jikwon <jikwon@student.42seoul.kr>
2022-03-13 16:07:23 +0900

    delete old files

--- backend/src/config/redirect_url.ts
+++
@@ -1,7 +0,0 @@
-import { FRONTADDR } from "./const"
-
-const frontSignupPage = FRONTADDR + "/nickandprofile"
-const frontLobyPage = FRONTADDR + "/game"
-const frontTwoFactorAuthenticationPage = FRONTADDR + "/2fa"
-
-export {frontSignupPage, frontLobyPage, frontTwoFactorAuthenticationPage}
--- backend/src/socketEvents/user/dto/newFriend.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsBoolean } from "class-validator"
-import { UserInfoDTO } from "./userInfo.dto"
-
-export class NewFriendDTO extends UserInfoDTO {
-    @IsBoolean()
-    result : boolean;
-}
