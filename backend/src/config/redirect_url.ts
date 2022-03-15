00fd9d4dcb7344a460a09d7e94ed94b63d0bc362
jikwon <jikwon@student.42seoul.kr>
2022-03-15 14:32:37 +0900

    chat content -> contents & add updateGameRoom event

---
+++ backend/src/config/redirect_url.ts
@@ -0,0 +1,7 @@
+import { FRONTADDR } from "./const"
+
+const frontSignupPage = FRONTADDR + "/nickandprofile"
+const frontLobyPage = FRONTADDR + "/game"
+const frontTwoFactorAuthenticationPage = FRONTADDR + "/2fa"
+
+export {frontSignupPage, frontLobyPage, frontTwoFactorAuthenticationPage}
