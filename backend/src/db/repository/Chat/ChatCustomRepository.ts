12ac0846fbcecbb9598d0a70bc94fe6f139d5fa3
jikwon <jikwon@student.42seoul.kr>
2022-03-15 14:32:03 +0900

    delete old files

--- backend/src/db/repository/Chat/ChatCustomRepository.ts
+++
@@ -1,5 +0,0 @@
-import { ChatMembershipRepository } from "./ChatMembership.repository";
-import { ChatRoomRepository } from "./ChatRoom.repository";
-import { ChatBanListRepository } from "./ChatBanListRepository";
-import { ChatHistoryRepository } from "./ChatHistoryRepository";
-export {ChatMembershipRepository, ChatRoomRepository, ChatBanListRepository, ChatHistoryRepository};
--- backend/src/socketEvents/online/onlineGameMap.ts
+++
@@ -1,3 +0,0 @@
-import { Game } from "src/socketEvents/game/gameElement/game";
-
-export const onlineGameMap : { [index : string] : Game } = {};
