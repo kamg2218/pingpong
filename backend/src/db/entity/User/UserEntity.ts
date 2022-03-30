4c86bfb9d50e11cbcad0644ad2b81400a093e4f5
윤현지 <hyoon@student.42seoul.kr>
2022-03-30 14:44:04 +0900

    :rocket: update main branch

---
+++ backend/src/db/entity/User/UserEntity.ts
@@ -0,0 +1,6 @@
+import { BlockedFriends } from './BlockedFriends.entity'
+import { Friends } from './Friends.entity'
+import { RestrictedList } from './RestrictedList.entity'
+import { User } from './User.entity'
+
+export { BlockedFriends, Friends, RestrictedList, User }
