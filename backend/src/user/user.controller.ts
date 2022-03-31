176f592a07e222063c7f6e77266c469327b373e0
윤현지 <hyoon@student.42seoul.kr>
2022-03-31 14:10:46 +0900

    :white_check_mark: update ProfileModal and MyProfileModal

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query, UnauthorizedException } from "@nestjs/common"
+import { JwtService } from "@nestjs/jwt";
+import { AuthService } from "src/auth/auth.service";
+import { UserService } from "./user.service";
+import { Cookies } from "src/auth/cookies.decorator";
+
+@Controller('user')
+export class UserController {
+    constructor(
+        private readonly userService : UserService,
+        private readonly jwtService : JwtService,
+        private readonly authService : AuthService
+    
+    ) {}
+
+    @Get('/check')
+    async checkState(@Query('url') urlvalue : any, @Cookies('accessToken') at : string) {
+        try {
+            const payload = await this.jwtService.verify(at);
+            const user = await this.authService.validate2FAJwt(payload);
+            let state : string;
+            if (user.isTwoFactorAuthenticationEnabled && !payload.isSecondFactorAuthenticated)
+                state = "2fa";
+            else
+                state = await this.userService.findState(user);
+            return {state};
+        }
+        catch (e) {
+            if (urlvalue === "main") {
+                return {state : "logout"};  
+            }
+            else {
+                throw new UnauthorizedException();
+            }
+                
+        }
+    }
+}
