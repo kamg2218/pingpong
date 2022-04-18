9e9a749ea416bb2405f7fe5ef8ef1599f45d0d0b
윤현지 <hyoon@student.42seoul.kr>
2022-04-19 01:14:21 +0900

    :white_check_mark: add modal button in chatBox

---
+++ backend/src/socketEvents/online/onlineChatRoom.ts
@@ -0,0 +1,83 @@
+import { Server } from "socket.io";
+import { User } from "src/db/entity/User/UserEntity";
+import { BlockedFriendsRepository, UserRepository } from "src/db/repository/User/UserCustomRepository";
+import { AuthSocket } from "src/type/AuthSocket.interface";
+import { getCustomRepository } from "typeorm";
+import { Emitter } from "../auth/emitter";
+import { onlineManager } from "./onlineManager";
+
+export class onlineChatRoom {
+    static _server : Server = null;
+    private roomid : string;
+    private members : string[];
+    private emitter;
+    private server;
+    
+    get id() {
+        return this.roomid;
+    }
+
+    static init(server : Server) {
+        if (!onlineChatRoom._server)
+            onlineChatRoom._server = server;
+    }
+
+    constructor(roomid : string) {
+        this.roomid = roomid;
+        this.members = [];
+        this.server = onlineChatRoom._server;
+        this.emitter = new Emitter(this);
+    }
+
+    public async sayToRoom(socket : AuthSocket, payload : any) {
+        const repo_blockList = getCustomRepository(BlockedFriendsRepository);
+        const repo_user = getCustomRepository(UserRepository);
+        let user = await repo_user.findOne(socket.userid);
+        this.members.map(async (socketid) => {
+            let theOtherId = onlineManager.userIdOf(socketid);
+            console.log(`[saytoRoom] Sent to ${theOtherId}`)
+            if (!await repo_blockList.amIBlockedByid(user, theOtherId))
+                this.emitter.emitById(socketid, "chatMessage", {
+                    chatid : this.id,
+                    userid : socket.userid,
+                    contents : payload.contents,
+                    createDate : payload.time
+                });
+        });
+    }
+
+    public announceExceptMe(mySocketId : string, event : string, payload : any) {
+        console.log("[AEM] members :", this.members);
+        this.members.map((socketid)=>{
+            if (socketid !== mySocketId)
+                this.emitter.emitById(socketid, event, payload);
+                // onlineChatRoom.server.to(socketid).emit(event, payload);
+        });
+    }
+
+    public announce(event : string, payload : any) {
+        console.log("[AN] members :", this.members);
+        this.members.map((socketid)=>{
+            let userid = onlineManager.userIdOf(socketid);
+            console.log(`[announce] Sent to ${userid}`);
+            this.emitter.emitById(socketid, event, payload);
+            // onlineChatRoom.server.to(socketid).emit(event, payload);
+        });
+    }
+
+    public join(socketid: string) {
+        console.log(`ONline chatroom joined : ${socketid}`);
+        if (socketid)
+            this.members.push(socketid);
+        console.log("now members : ", this.members );
+    }
+
+    public leave(socketid : string) {
+        if (!socketid)
+            return ;
+        const index = this.members.indexOf(socketid);
+        if (index !== -1)
+            this.members.splice(index, 1);
+    }
+    
+}
