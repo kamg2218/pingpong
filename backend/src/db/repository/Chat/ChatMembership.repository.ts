b9318b87d825013783bbd4205a31743e9fd5d32e
윤현지 <hyoon@student.42seoul.kr>
2022-04-19 02:06:32 +0900

    :building_construction: add inviteGameRoom apils

---
+++ backend/src/db/repository/Chat/ChatMembership.repository.ts
@@ -0,0 +1,77 @@
+import { ChatRoom, ChatMembership } from "src/db/entity/Chat/ChatEntity";
+import { User } from "src/db/entity/User/UserEntity";
+import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { ChatRoomRepository } from "./ChatCustomRepository";
+
+
+@EntityRepository(ChatMembership)
+export class ChatMembershipRepository extends Repository<ChatMembership> {
+
+    createChatMemberInfo(user: User, chatroom: ChatRoom) : ChatMembership{
+        let member = this.create();
+        member.chatroom = chatroom;
+        member.member = user;
+        member.position = "normal";
+        return member;
+    }
+
+    createChatOwnerInfo(user: User, chatroom: ChatRoom) : ChatMembership {
+        let member = this.create();
+        member.chatroom = chatroom;
+        member.member = user;
+        member.position = "owner";
+        return member;
+    }
+
+    async setAsNormal(userid : string) {
+        const membership = await this.findOne({member : {userid : userid}});
+        if (membership && membership.position === "manager") {
+            await this.update(membership.index, {position : "normal", promotedDate : null});
+            return true;
+        }
+        return false;
+    }
+
+    async setAsManager(userid : string) {
+        const chatuser = await this.findOne({member : {userid : userid}});
+        if (chatuser && chatuser.position === "normal") {
+            await this.update(chatuser.index, {position: "manager", promotedDate : new Date()});
+            return true;
+        }
+        return false;
+    }
+
+    async deleteChatUser(userid : string, chatid: string) {
+        const repo_chatRoom = getCustomRepository(ChatRoomRepository);
+        const chatroom = await repo_chatRoom.findOne({chatid: chatid});
+        let deleteuser = await this.findOne({chatroom:{chatid: chatid}, member: {userid: userid}});
+        await this.delete(deleteuser.index);
+        await repo_chatRoom.update(chatid, {memberCount: chatroom.memberCount - 1})
+    }
+
+    async getNewOwner(chatid : string) {
+        let newOwner;
+        const managers = await this.find({
+            where : {chatroom : {chatid : chatid}, position : "manager"},
+            order : {promotedDate : "ASC"}
+        });
+        if (managers.length)
+            newOwner = managers[0];
+        else {
+            newOwner = await this.findOne({
+                where : {chatroom : {chatid : chatid}},
+                order : {enterDate : "ASC"}
+            });
+        }
+        return newOwner;
+    }
+    // 내가 속해있는 챗팅방의 chatid를 배열로 return
+    async getMyChatRoom(userid : string) {
+        let chatList = [];
+        let allChatList = await this.find({member : {userid: userid}});
+        for (let index in allChatList) {
+            chatList.push(allChatList[index].chatroom);
+        };
+        return chatList;
+    }
+}
---
+++ backend/src/db/repository/User/Friends.repository.ts
@@ -0,0 +1,62 @@
+import { Friends } from "src/db/entity/User/Friends.entity";
+import { User } from "src/db/entity/User/User.entity";
+import { EntityRepository, Repository } from "typeorm";
+
+@EntityRepository(Friends)
+export class FriendsRepository extends Repository<Friends> {
+
+    async isMyFriend(me : User, other : User) : Promise<boolean> {
+        const res = await this.find({where : [{requestFrom : me, requestTo : other, requestStatus : 'friend'}, {requestTo : me, requestFrom : other, requestStatus : 'friend'}]});
+        if (!res.length)
+            return false;
+        return true;
+    }
+
+    async isNotMyFriend(me : User, other : User) : Promise<boolean> {
+        const res = await this.find({where : [{requestFrom : me, requestTo : other, requestStatus : 'friend'}, {requestTo : me, requestFrom : other, requestStatus : 'friend'}]});
+        if (!res.length)
+            return true;
+        return false;
+    }
+    async didISendRequest(me : User, theOther : User) : Promise<boolean> {
+        const result : Friends[] = await this.find({requestFrom : me, requestTo : theOther})
+        if (result.length)
+            return true;
+        return false;
+    }
+
+    async sendFriendRequest(me : User, theOther : User) {
+        if (await this.didISendRequest(me, theOther))
+            return ;
+        await this.insert({requestFrom : me, requestTo : theOther})
+    }
+
+    async friends(me : User) {
+        return await this.find({where : [{requestFrom : me, requestStatus : "friend"}, {requestTo : me, requestStatus : "friend"}]});
+    }
+    
+    async receivedFriendRequest(me : User, theOther? : User) {
+        const res = theOther ? this.find({requestTo : me, requestFrom : theOther, requestStatus : 'waiting'}) : this.find({requestTo : me, requestStatus : 'waiting'});
+        return res;
+    }
+
+    async cancleSentFriendRequest(me : User, theOther : User) {
+        await this.delete({ requestFrom : me, requestTo : theOther, requestStatus : 'waiting'})
+    }
+
+    async deleteFriendRelation(me : User, theOther : User) {
+        const request = await this.find({where : [{requestFrom : me, requestTo : theOther}, {requestTo : me, requestFrom : theOther}]});
+        if (request)
+            this.remove(request);
+    }
+
+    async acceptFriendRequest(from : User, to : User) {
+        this.update({requestFrom : from, requestTo : to}, {requestStatus : 'friend'});
+    }
+
+    async rejectFriendRequest(from : User, to : User) {
+        const request = await this.find({requestFrom : from, requestTo : to});
+        if (request)
+            this.remove(request);
+    }
+}
