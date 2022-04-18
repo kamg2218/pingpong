8f1f5378022ebc95299def9aaf0384a07bd74c2b
윤현지 <hyoon@student.42seoul.kr>
2022-04-18 14:59:56 +0900

    :bug: update user check api

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Query, UnauthorizedException } from "@nestjs/common"
+import { JwtService } from "@nestjs/jwt";
+import { AuthService } from "src/auth/auth.service";
+import { UserService } from "./user.service";
+import { Cookies } from "src/auth/cookies.decorator";
+
+@Controller('user')
+export class UserController {
+    constructor(
+        private readonly userService : UserService,
+        private readonly jwtService : JwtService,
+        private readonly authService : AuthService
+    
+    ) {}
+
+    @Get('/check')
+    async checkState(@Query('url') urlvalue : any, @Cookies('accessToken') at : string) {
+        try {
+            const payload = await this.jwtService.verify(at);
+            const user = await this.authService.validate2FAJwt(payload);
+            let state : string;
+            if (user.isTwoFactorAuthenticationEnabled && !payload.isSecondFactorAuthenticated)
+                state = "2fa";
+            else
+                state = await this.userService.findState(user);
+            console.log("state : ", state);
+            return {state};
+            // return state;
+        }
+        catch (e) {
+            if (urlvalue === "main") {
+                console.log("state : logout");
+                return {state : "logout"};
+                // return "logout"
+            }
+            else {
+                console.log("state : 401");
+                throw new UnauthorizedException();
+            }
+                
+        }
+    }
+}
