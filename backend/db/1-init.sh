0f83a362e76c4b2d740a57b96ca00df576e7d3af
윤현지 <yunhyeonji@yunhyeonjiui-MacBookPro.local>
2022-04-19 17:17:37 +0900

    db init

--- backend/db/1-init.sh
+++ backend/db/1-init.sh
@@ -4,3 +4,5 @@ export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER <<-EOSQL
 	CREATE DATABASE $POSTGRES_DB;
 EOSQL
+
+echo "host all  all all trust" >> /var/lib/postgresql/data/pg_hba.conf
