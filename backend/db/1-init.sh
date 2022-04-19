124bc224eb1b4fe7e741e68c04becbff8ff32d42
윤현지 <yunhyeonji@yunhyeonjiui-MacBookPro.local>
2022-04-20 00:33:36 +0900

    temporary build front scr outside

--- backend/db/1-init.sh
+++ backend/db/1-init.sh
@@ -4,5 +4,3 @@ export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER <<-EOSQL
 	CREATE DATABASE $POSTGRES_DB;
 EOSQL
-
-echo "host all  all all trust" >> /var/lib/postgresql/data/pg_hba.conf
