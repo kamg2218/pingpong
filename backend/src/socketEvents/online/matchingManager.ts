dcff268204c51b6090f471bef7c87e616d5e3173
윤현지 <hyoon@student.42seoul.kr>
2022-04-19 16:07:07 +0900

    :memo: remove console logs

---
+++ backend/src/socketEvents/online/matchingManager.ts
@@ -0,0 +1,43 @@
+import { Queue } from "../game/gameElement/queue";
+
+const waitingMatchingList = new Queue();
+
+export const MatchingManager = {
+    isThereWaitingUser() : boolean {
+        if (waitingMatchingList.size())
+            return true;
+        return false;
+    },
+
+    getOne() : string {
+        return waitingMatchingList.pop();
+    },
+
+    putOnTheWaitingList(userid : string) {
+        waitingMatchingList.push(userid);
+    },
+
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    },
+
+    generateGameRoomOptions() {
+        let title = "random" + this.getRandomInt(0, 10000);
+        return {
+            title,
+            type : "public",
+            maxObsCount : 5,
+            speed : 1
+        };
+    },
+
+    cancle(userid : string) {
+        waitingMatchingList.delete(userid);
+    },
+
+    print(){
+        console.log("waiting list : ", waitingMatchingList)
+    }
+};
